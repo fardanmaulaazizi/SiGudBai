bea3bf58c14645ec2a1eb8bd887763adc575cf81
fardanmaulaazizi <fardanm.azizi11@gmail.com>
2024-10-01 14:41:33 +0700

    Bug Fixing Main Page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,32 +38,31 @@
         </ul>
     </nav>
     {{-- Slider --}}
-    <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+    <div id="carousel" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img src="/images/banner1.png" class="d-block w-100 object-fit-cover" height="450px"
+                <img src="/images/banner1.png" class="d-block w-100" style="height: 450px; object-fit: cover;"
                     alt="Banner Pasar Cabai">
             </div>
             <div class="carousel-item">
-                <img src="/images/banner2.png" class="d-block w-100 object-fit-cover" height="450px"
+                <img src="/images/banner2.png" class="d-block w-100" style="height: 450px; object-fit: cover;"
                     alt="Banner Pasar Cabai">
             </div>
             <div class="carousel-item">
-                <img src="/images/banner3.png" class="d-block w-100 object-fit-cover" height="450px"
+                <img src="/images/banner3.png" class="d-block w-100" style="height: 450px; object-fit: cover;"
                     alt="Banner Pasar Cabai">
             </div>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying"
-            data-bs-slide="prev">
+        <button class="carousel-control-prev" type="button" data-bs-target="#carousel" data-bs-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Previous</span>
         </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying"
-            data-bs-slide="next">
+        <button class="carousel-control-next" type="button" data-bs-target="#carousel" data-bs-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Next</span>
         </button>
     </div>
+
 
     <section id="produk">
         <div class="row" style="padding:80px">
